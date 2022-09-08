d1d8b3c2fdd1354dbf20604699dbee1a4ac42cd3
vl_ad <vover@mail.ru>
2022-09-08 15:06:08 +0300

    управление слайдером и кнопкой с ардуино

--- karelia_unity/Assets/Scripts/Test/ArduinoTestButton.cs
+++ karelia_unity/Assets/Scripts/Test/ArduinoTestButton.cs
@@ -6,17 +6,29 @@ using UnityEngine.UI;
 public class ArduinoTestButton : MonoBehaviour
 {
     [SerializeField] private Text arduinoOutpuText;
-    
+    [SerializeField] private Slider _slider;
+
+    //private variables
+    private UduinoManager  _uduino;
+
+    private void Awake()
+    {
+        _uduino = UduinoManager.Instance;
+    }
 
     void Start()
     {
-        UduinoManager.Instance.pinMode(11, PinMode.Input_pullup);
+        _uduino.pinMode(11, PinMode.Input_pullup);
+        _uduino.pinMode(AnalogPin.A1, PinMode.Input);
     }
 
     void Update()
     {
-        var res = UduinoManager.Instance.digitalRead(11);
-        arduinoOutpuText.text = ""+res;
+        var buttonCondition = _uduino.digitalRead(11);
+        var potentiometerCondition = _uduino.analogRead(AnalogPin.A1);
+
+        arduinoOutpuText.text = ""+buttonCondition;
+        _slider.value = (float)potentiometerCondition / 1000.0f;
     }
    
 
