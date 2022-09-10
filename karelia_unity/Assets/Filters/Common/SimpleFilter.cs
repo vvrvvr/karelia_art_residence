590be96d84f577dae25f7bbcc5d9bd8ddf75d578
vl_ad <vover@mail.ru>
2022-09-10 13:08:27 +0300

    Добавил фильтры на изображение

---
+++ karelia_unity/Assets/Filters/Common/SimpleFilter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SimpleFilter : MonoBehaviour
+{
+    [SerializeField] private Shader _shader;
+
+    protected Material _mat;
+
+    private bool _useFilter = true;
+
+    private void Awake()
+    {
+        _mat = new Material(_shader);
+        Init();
+    }
+
+    protected virtual void Init()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            _useFilter = !_useFilter;
+        }
+
+        OnUpdate();
+    }
+
+    protected virtual void OnUpdate()
+    {
+
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (_useFilter)
+        {
+            UseFilter(src, dst);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
+    }
+
+    protected virtual void UseFilter(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, _mat);
+    }
+}
