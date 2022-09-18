0d6765f00cb4b4b7e74c6611a3dc5089d858fed2
vl_ad <vover@mail.ru>
2022-09-18 19:45:48 +0300

    ардуино менеджер wip

--- karelia_unity/Assets/Gadalka/Scripts/ArduinoManager.cs
+++ karelia_unity/Assets/Gadalka/Scripts/ArduinoManager.cs
@@ -13,18 +13,17 @@ public class ArduinoManager : MonoBehaviour
     private bool isBoardConnected = true;
 
     //potentiometers 
-    [HideInInspector] public int p1Current;
+    private int[] potentiometersArr = new int[6];
     [HideInInspector] public int p1Prev = 0;
-    [HideInInspector] public int p2Current;
     [HideInInspector] public int p2Prev = 0;
-    [HideInInspector] public int p3Current;
     [HideInInspector] public int p3Prev = 0;
-    [HideInInspector] public int p4Current;
     [HideInInspector] public int p4Prev = 0;
-    [HideInInspector] public int p5Current;
     [HideInInspector] public int p5Prev = 0;
-    private int HorizontalControl;
-    private int VerticalControl;
+    public int HorizontalControl;
+    public int VerticalControl;
+
+    public int vertIndex;
+    public int horIndex;
 
 
     //buttons
@@ -32,8 +31,7 @@ public class ArduinoManager : MonoBehaviour
     [HideInInspector] public int b1Prev = 0;
 
     //крабики хз как по английски
-    [HideInInspector] public int k1Current;
-    [HideInInspector] public int k1Prev = 0;
+    [HideInInspector] public int k0Prev = 0;
 
 
     private void Awake()
@@ -56,7 +54,7 @@ public class ArduinoManager : MonoBehaviour
         _uduino.pinMode(AnalogPin.A0, PinMode.Input);
 
         //TEMP
-        SetControls();
+        SetControls(true);
     }
 
     void Update()
@@ -64,30 +62,30 @@ public class ArduinoManager : MonoBehaviour
         if (isBoardConnected)
         {
             //potentiometers
-            p1Current = _uduino.analogRead(AnalogPin.A1);
-            p2Current = _uduino.analogRead(AnalogPin.A2);
-            p3Current = _uduino.analogRead(AnalogPin.A3);
-            p4Current = _uduino.analogRead(AnalogPin.A4);
-            p5Current = _uduino.analogRead(AnalogPin.A5);
+            potentiometersArr[1] = _uduino.analogRead(AnalogPin.A1);
+            potentiometersArr[2] = _uduino.analogRead(AnalogPin.A2);
+            potentiometersArr[3] = _uduino.analogRead(AnalogPin.A3);
+            potentiometersArr[4] = _uduino.analogRead(AnalogPin.A4);
+            potentiometersArr[5] = _uduino.analogRead(AnalogPin.A5);
 
             //buttons
             b1Current = _uduino.digitalRead(7);
 
             //krab
-            k1Current = _uduino.analogRead(AnalogPin.A0);
+            potentiometersArr[0] = _uduino.analogRead(AnalogPin.A0);
 
 
             //секция манипуляций
             UpdateVals();
-            //
 
-            p1Prev = p1Current;
-            p1Prev = p1Current;
-            p1Prev = p1Current;
-            p1Prev = p1Current;
-            p1Prev = p1Current;
+            // set previous values
+            p1Prev = potentiometersArr[1];
+            p2Prev = potentiometersArr[2];
+            p3Prev = potentiometersArr[3];
+            p4Prev = potentiometersArr[4];
+            p5Prev = potentiometersArr[5];
             b1Prev = b1Current;
-            k1Prev = k1Current;
+            k0Prev = potentiometersArr[0];
         }
         else
         {
@@ -95,18 +93,37 @@ public class ArduinoManager : MonoBehaviour
         }
     }
 
-    public void SetControls()
+    public void SetControls(bool isRandom)
     {
-
+        if (isRandom)
+        {
+            vertIndex = Random.Range(1, potentiometersArr.Length);
+            horIndex = Random.Range(1, potentiometersArr.Length);
+            if (vertIndex == horIndex)
+            {
+                while (vertIndex == horIndex)
+                {
+                    horIndex = Random.Range(1, potentiometersArr.Length);
+                }
+            }
+        }
+        else
+        {
+            vertIndex = 1;
+            horIndex = 2;
+        }
     }
 
     public void UpdateVals()
     {
-
+        HorizontalControl = potentiometersArr[horIndex];
+        VerticalControl = potentiometersArr[vertIndex];
     }
 
     public void ResetGame()
     {
         _display2Manager.ResetDisplay2();
+        SetControls(true);
+        //reset display 1
     }
 }
