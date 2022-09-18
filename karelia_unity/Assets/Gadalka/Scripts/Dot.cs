7b237f5cba1e9fe91cd06f9415215961a8dd1a13
vl_ad <vover@mail.ru>
2022-09-18 13:27:14 +0300

    передвижение точки (два рейкаста)

--- karelia_unity/Assets/Gadalka/Scripts/Dot.cs
+++ karelia_unity/Assets/Gadalka/Scripts/Dot.cs
@@ -43,13 +43,127 @@ public class Dot : MonoBehaviour
 
         if (moveValue > 0)
         {
-            Move(Vector3.up, moveValue);
+            MoveUpDown(Vector3.up, moveValue);
         }
         if (moveValue < 0)
-            Move(Vector3.down, moveValue);
+            MoveUpDown(Vector3.down, moveValue);
 
 
         prevInt = currentInt;
+    }
+
+    private void MoveRightLeft(Vector3 dir, int moveVal)
+    {
+        Vector3 newPos = transform.position + dir * Time.deltaTime * moveVal * factor;
+        float dirLength = Mathf.Abs(newPos.x - transform.position.x);
+        Vector3 upRayPoint = new Vector3(transform.position.x, transform.position.y + halfDotDimention, transform.position.z);
+        Vector3 downRayPoint = new Vector3(transform.position.x, transform.position.y - halfDotDimention, transform.position.z);
+        RaycastHit hit;
+
+        if (Physics.Raycast(upRayPoint, dir, out hit, dirLength, wallLayer))
+        {
+            var point = new Vector3(0f, 0f, 0f);
+            switch (dir)
+            {
+                case Vector3 v when v.Equals(Vector3.right):
+                    point = hit.point - new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(point.x, transform.position.y, point.z);
+                    transform.position = point;
+                    break;
+                case Vector3 v when v.Equals(Vector3.left):
+                    point = hit.point + new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(point.x, transform.position.y, point.z);
+                    transform.position = point;
+                    break;
+            }
+        }
+        else if (Physics.Raycast(downRayPoint, dir, out hit, dirLength, wallLayer))
+        {
+            var point = new Vector3(0f, 0f, 0f);
+            switch (dir)
+            {
+                case Vector3 v when v.Equals(Vector3.right):
+                    point = hit.point - new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(point.x, transform.position.y, point.z);
+                    transform.position = point;
+                    break;
+                case Vector3 v when v.Equals(Vector3.left):
+                    point = hit.point + new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(point.x, transform.position.y, point.z);
+                    transform.position = point;
+                    break;
+            }
+        }
+        else
+        {
+            switch (dir)
+            {
+                case Vector3 v when v.Equals(Vector3.right):
+                    transform.position += dir * Time.deltaTime * moveValue * factor;
+                    break;
+                case Vector3 v when v.Equals(Vector3.left):
+                    transform.position -= dir * Time.deltaTime * moveValue * factor;
+                    break;
+            }
+        }
+
+    }
+
+    private void MoveUpDown(Vector3 dir, int moveVal)
+    {
+        Vector3 newPos = transform.position + dir * Time.deltaTime * moveVal * factor;
+        float dirLength = Mathf.Abs(newPos.y - transform.position.y);
+        Vector3 leftRayPoint = new Vector3(transform.position.x - halfDotDimention, transform.position.y, transform.position.z);
+        Vector3 rightRayPoint = new Vector3(transform.position.x + halfDotDimention, transform.position.y, transform.position.z);
+        RaycastHit hit;
+
+        if (Physics.Raycast(leftRayPoint, dir, out hit, dirLength, wallLayer))
+        {
+            var point = new Vector3(0f, 0f, 0f);
+            switch (dir)
+            {
+                case Vector3 v when v.Equals(Vector3.up):
+                    point = hit.point - new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(point.x, transform.position.y, point.z);
+                    transform.position = point;
+                    break;
+                case Vector3 v when v.Equals(Vector3.down):
+                    point = hit.point + new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(point.x, transform.position.y, point.z);
+                    transform.position = point;
+                    break;
+            }
+        }
+        else if (Physics.Raycast(rightRayPoint, dir, out hit, dirLength, wallLayer))
+        {
+            var point = new Vector3(0f, 0f, 0f);
+            switch (dir)
+            {
+                case Vector3 v when v.Equals(Vector3.up):
+                    point = hit.point - new Vector3(0f, halfDotDimention, 0f);
+                    point = new Vector3(transform.position.x, point.y, point.z);
+                    transform.position = point;
+                    break;
+                case Vector3 v when v.Equals(Vector3.down):
+                    point = hit.point + new Vector3(halfDotDimention, 0f, 0f);
+                    point = new Vector3(transform.position.x, point.y, point.z);
+                    transform.position = point;
+                    break;
+            }
+        }
+        else
+        {
+            switch (dir)
+            {
+                case Vector3 v when v.Equals(Vector3.up):
+                    transform.position += dir * Time.deltaTime * moveValue * factor;
+                    break;
+                case Vector3 v when v.Equals(Vector3.down):
+                    transform.position -= dir * Time.deltaTime * moveValue * factor;
+                    break;
+            }
+        }
+
     }
 
     private void Move(Vector3 dir, int moveVal)
@@ -58,7 +172,7 @@ public class Dot : MonoBehaviour
         float dirLength = Mathf.Abs(newPos.x - transform.position.x);
         if (Physics.Raycast(transform.position, dir, out RaycastHit hit, dirLength, wallLayer))
         {
-            var point = new Vector3(0f,0f,0f);
+            var point = new Vector3(0f, 0f, 0f);
             switch (dir)
             {
                 case Vector3 v when v.Equals(Vector3.right):
